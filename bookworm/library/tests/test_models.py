95b430dcb2b2dd9cb1fd64477a3a35b1798e20e1
abdelazer@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-06-14 00:45:43 +0000

    Adding a tiny valid and invalid sample .epub; tests for same, an OPF file without a dc:creator; tests for same and a general increase in tests for models.py
    Tests now runnable under 'nose'
    
    --HG--
    rename : bookworm/library/tests.py => bookworm/library/tests/test_models.py
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40154

--- bookworm/library/tests/test_models.py
+++ bookworm/library/tests/test_models.py
@@ -1,9 +1,17 @@
-import sys, os, time
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import re
+import sys
+import time
+import unittest
+
 from os.path import isfile, isdir
+
 sys.path.append('/usr/local/google_appengine')
 sys.path.append('/usr/local/google_appengine/lib/yaml/lib')
 
-import unittest
 from google.appengine.api import apiproxy_stub_map
 from google.appengine.api import datastore_file_stub
 from google.appengine.api import mail_stub
@@ -11,12 +19,14 @@ from google.appengine.api import urlfetch_stub
 from google.appengine.api import user_service_stub
 from google.appengine.api import users
 
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-dir_path = '%s/..' % os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+# Could we get this with the relative imports in 2.5 __future__?
+dir_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 sys.path.append(dir_path)
-from models import *
-import views
+
+from library.models import *
+import library.views as views
 
 
 logging.basicConfig(level=logging.INFO)
@@ -27,7 +37,7 @@ AUTH_DOMAIN = 'gmail.com'
 LOGGED_IN_USER = 'test@example.com'
 
 # Data for public epub documents
-DATA_DIR = 'tests/data'
+DATA_DIR = os.path.abspath('./data')
 
 # Local documents should be added here and will be included in tests,
 # but not in the svn repository
@@ -65,8 +75,11 @@ class TestModels(unittest.TestCase):
 
         if isdir(PRIVATE_DATA_DIR):
             self.documents += [d for d in os.listdir(PRIVATE_DATA_DIR) if '.epub' in d and isfile('%s/%s' % (PRIVATE_DATA_DIR, d))] 
+        
+
        
     def testTest(self):
+        # what does this test?
         b = HTMLFile(idref='test')
         b.put()
 
@@ -78,8 +91,12 @@ class TestModels(unittest.TestCase):
     def testGetAllDocuments(self):
         '''Run through all the documents at a high level'''
         for d in self.documents:
-            doc = self.create_document(d)
-            doc.explode()
+            if d.startswith("invalid"):
+                # Test bad documents here?
+                pass
+            else:
+                doc = self.create_document(d)
+                doc.explode()
 
     def testGetTitle(self):
         '''Did we get back the correct title?'''
@@ -89,7 +106,7 @@ class TestModels(unittest.TestCase):
         document.explode()
         self.assertEquals(title, document.title)
 
-    def testGetSingleAuthor(self):
+    def testSingleAuthor(self):
         '''Did we get a single author from our author() method?'''
         author = u'Jane Austen'
         filename = 'Pride-and-Prejudice_Jane-Austen.epub'
@@ -99,19 +116,42 @@ class TestModels(unittest.TestCase):
 
     def testGetMultipleAuthors(self):
         '''Do we return the correct number of authors in the correct order?'''
-        test_authors = [u'First Author', u'Second Author']
+        expected_authors = [u'First Author', u'Second Author']
         opf_file = 'two-authors.opf'
         document = MockEpubArchive(name=opf_file)
         opf = document.xml_from_string(open('%s/%s' % (DATA_DIR, opf_file)).read())
         authors = document.get_authors(opf)
-        self.assertEquals(authors, test_authors)
+        self.assertEquals(expected_authors, authors)
+
+    def testGetMultipleAuthorsAsAuthor(self):
+        '''Multiple authors should be displayable in a short space.'''
+        opf_file = 'two-authors.opf'
+        expected_authors = [u'First Author', u'Second Author']
+        document = MockEpubArchive(name=opf_file)
+        opf = document.xml_from_string(open('%s/%s' % (DATA_DIR, opf_file)).read())
+        
+        fuzz = 4
+        len_first_author = len(expected_authors[0])
+        len_short_author_str = len(document.get_author(opf))
+        difference = len_short_author_str - len_first_author
+        self.assert_(difference < 4)
+
+    def testNoAuthor(self):
+        '''An OPF document with no authors should return None.'''
+        no_author_opf_file = 'no-author.opf'
+        no_author_document = MockEpubArchive(name=no_author_opf_file)
+        opf = no_author_document.xml_from_string(open('%s/%s' % (DATA_DIR, no_author_opf_file)).read())
+
+        author = no_author_document.get_author(opf)
+        self.assertEquals(None, author)
 
     def testCreateDocument(self):
-        '''Assert that we created a non-None document'''
+        '''Assert that we created a non-None document.'''
         d = self.create_document(self.documents[0])
         self.assert_(d)
 
     def testFindDocument(self):
+        """Documents should be findable by title and author."""
         author = u'Jane Austen'
         title = u'Pride and Prejudice'
         filename = 'Pride-and-Prejudice_Jane-Austen.epub'
@@ -121,6 +161,20 @@ class TestModels(unittest.TestCase):
         logging.info(users.get_current_user())
         d = _get_document(title, author)
         self.assert_(d)
+
+    def testBadEpubFails(self):
+        """ePub documents with missing compontent should raise errors."""
+        filename = 'invalid_no_container.epub'
+        document = self.create_document(filename)
+        self.assertRaises(InvalidEpubException, document.explode)
+
+    def testSafeName(self):
+        """Names should be safely quoted for URLs."""
+        name = u'John Q., CommasAreForbidden'
+        sn = safe_name(name)
+        comma_re = re.compile(",")
+        result = comma_re.match(sn)
+        self.failIf(result)
 
     def create_document(self, document):
         epub = MockEpubArchive(name=document)
@@ -147,6 +201,10 @@ class MockEpubArchive(EpubArchive):
 
     def xml_from_string(self, string):
         return self._xml_from_string(string)
+
+    def get_author(self, opf):
+        self.authors = self._get_authors(opf)
+        return self.author()
 
     def get_authors(self, opf):
         return self._get_authors(opf)
