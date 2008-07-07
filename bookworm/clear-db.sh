4128538f4ffcc4df9c8d162a5b5149cbea63f682
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-07-07 13:48:42 +0000

    More advanced db reset script
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40223

--- bookworm/clear-db.sh
+++ bookworm/clear-db.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-./manage.py sqlclear library > clear.sql
+if [ "$1" = "" ]; then
+    echo "Clearing library"
+    ./manage.py sqlclear library > clear.sql
+fi
+if [ "$1" = "auth" ]; then
+    echo "Clearing $1"
+    ./manage.py sqlclear auth > clear.sql
+fi
+if [ "$1" = "openid" ]; then
+    echo "Clearing $1"
+    ./manage.py sqlclear django_authopenid > clear.sql
+fi
+if [ "$1" = "all" ]; then
+    echo "Clearing $1"
+    ./manage.py sqlclear library auth django_authopenid > clear.sql
+fi
 mysql -u threepress --password=3press bookworm < clear.sql
 ./manage.py syncdb
