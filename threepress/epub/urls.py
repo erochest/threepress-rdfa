c170f8b5da915e2d1c3989e852ed12fe09b54625
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-10-07 16:58:26 +0000

    Small change required on live environment
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40364

--- threepress/epub/urls.py
+++ threepress/epub/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('threepress.epub.views',
-                       url(r'logos$', 'direct_to_template',
+                       url(r'/logos$', 'direct_to_template',
                            {'template': 'logos.html'}, name='logos'),
                        )
