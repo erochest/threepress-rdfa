8da29da54af2995e0cc485516c58d5ce7463ba9d
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-11-21 17:26:19 +0000

    Support for stanza [#83]
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40449

--- bookworm/middleware.py
+++ bookworm/middleware.py
@@ -4,9 +4,14 @@ from django.http import HttpResponsePermanentRedirect
 
 log = logging.getLogger('middleware')
 
+stanza_browsers = ('iphone', )
+
 class Mobile(object):
     @staticmethod
     def process_request(request):
         if 'HTTP_HOST' in request.META and request.META['HTTP_HOST'] != settings.MOBILE_HOST and request.mobile == True:
             log.info("Redirecting to %s because hostname was %s" %  (settings.MOBILE_HOST, request.META['HTTP_HOST']))
             return HttpResponsePermanentRedirect(settings.MOBILE_HOST)
+        for b in stanza_browsers:
+            if b in request.META["HTTP_USER_AGENT"].lower():
+                request.stanza_compatible = True
