3f2e372b5601664b5d730573afbdc355be56ec3e
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-11-21 17:18:40 +0000

    Corrections to middleware based on iphone testing
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40448

--- bookworm/middleware.py
+++ bookworm/middleware.py
@@ -1,8 +1,12 @@
+import logging
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect
+
+log = logging.getLogger('middleware')
 
 class Mobile(object):
     @staticmethod
     def process_request(request):
         if 'HTTP_HOST' in request.META and request.META['HTTP_HOST'] != settings.MOBILE_HOST and request.mobile == True:
+            log.info("Redirecting to %s because hostname was %s" %  (settings.MOBILE_HOST, request.META['HTTP_HOST']))
             return HttpResponsePermanentRedirect(settings.MOBILE_HOST)
