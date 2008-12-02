1136eb6e6d009bb0fd03c62d2e95c0143fc17706
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2008-12-02 17:22:07 +0000

    Working Stanza integration
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40458

--- bookworm/library/context_processors.py
+++ bookworm/library/context_processors.py
@@ -5,14 +5,24 @@ from library.forms import EpubValidateForm
 from library.models import SystemInfo, UserPref
 
 log = logging.getLogger('context_processors')
+
+stanza_browsers = ('iphone', )
  
 def nav(request):
     form = EpubValidateForm()
     return {'upload_form': form }
 
 def mobile(request):
-    return { 'mobile': settings.MOBILE }
+    stanza_compatible = False
+    if not hasattr(request, 'stanza_compatible') and request.META.has_key('HTTP_USER_AGENT'):
+        log.debug('Checking %s for Stanza-compatibility' % request.META['HTTP_USER_AGENT']) 
+        for b in stanza_browsers:
+            if b in request.META["HTTP_USER_AGENT"].lower():
+                log.debug('Setting true for stanza-compatible browser')
+                stanza_compatible = True
 
+    return { 'mobile': settings.MOBILE,
+             'stanza_compatible': stanza_compatible}
 
 def profile(request):
     '''Get (or create) a user preferences object for a given user.'''
--- bookworm/middleware.py
+++ bookworm/middleware.py
@@ -4,22 +4,9 @@ from django.http import HttpResponsePermanentRedirect
 
 log = logging.getLogger('middleware')
 
-stanza_browsers = ('iphone', )
-
 class Mobile(object):
     @staticmethod
     def process_request(request):
         if 'HTTP_HOST' in request.META and 'http://' + request.META['HTTP_HOST'] + '/' != settings.MOBILE_HOST and request.mobile == True:
             log.info("Redirecting to %s because hostname was %s" %  (settings.MOBILE_HOST, request.META['HTTP_HOST']))
             return HttpResponsePermanentRedirect(settings.MOBILE_HOST)
-
-        if not request.mobile:
-            return None
-        
-        if not hasattr(request, 'stanza_compatible') and request.META.has_key('HTTP_USER_AGENT'):
-            log.debug('Checking %s for Stanza-compatibility' % request.META['HTTP_USER_AGENT']) 
-            for b in stanza_browsers:
-                if b in request.META["HTTP_USER_AGENT"].lower():
-                    log.debug('Setting true for stanza-compatible browser')
-                    request.stanza_compatible = True
-                        
