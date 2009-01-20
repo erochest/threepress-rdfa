1456b5cf99d6286ac976e7e6b525360a3b94359f
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2009-01-20 23:10:12 +0000

    Add date metadata handling
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40511

--- bookworm/library/templatetags/bookworm.py
+++ bookworm/library/templatetags/bookworm.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from django import template
 
@@ -16,4 +17,34 @@ def last_chapter_read(context, document):
     chapter = document.get_last_chapter_read(user=context['user'])
     return { 'last_chapter_read':chapter,
              'document':document}
+
+
+@register.simple_tag
+def date_metadata(document, field):
+    '''Try some common date formats to get display in a Bookworm-style date,
+    otherwise give up.'''
+    metadata = document._get_metadata(field, document.opf) 
+    try:
+        try:
+            t = datetime.strptime(metadata, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            try:
+                t = datetime.strptime(metadata, "%Y-%m-%d")            
+            except ValueError:
+                return metadata
+        try:
+            datetime.strftime(t, "%A, %B %d %Y")
+        except ValueError:
+            return metadata
+    except:
+        return "Unknown"
+
+@register.simple_tag
+def extra_metadata(document, field):
+    '''Return any extra metadata with this field name.'''
+    try:
+        metadata = document._get_metadata(field, document.opf) 
+        return metadata
+    except:
+        return ""
 
