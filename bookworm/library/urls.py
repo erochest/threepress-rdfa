bc1fe31ca8d47cbf04ea4433adb9543ab83aea68
liza31337@e08d0fb5-4147-0410-a205-bba44f1f51a3 <none@none>
2009-02-17 22:07:24 +0000

    Allow .jpeg extension, grr
    --HG--
    extra : convert_revision : svn%3Ae08d0fb5-4147-0410-a205-bba44f1f51a3/trunk%40556

--- bookworm/library/urls.py
+++ bookworm/library/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('bookworm.library.views',
                        url(r'^reload/(?P<title>[^/]+)/(?P<key>\d+)/$', 'upload', name="reload"),                       
 
                        # Images from within documents
-                       url(r'^(view|chapter)/(?P<title>[^/]+)/(?P<key>\d+)/(first/|resume/)?(?P<image>.*(jpg|gif|png|svg)+)$', 
+                       url(r'^(view|chapter)/(?P<title>[^/]+)/(?P<key>\d+)/(first/|resume/)?(?P<image>.*(jpg|gif|png|svg|jpeg)+)$', 
                            'view_chapter_image', name="view_chapter_image"),                       
                        
                        # Document metadata
